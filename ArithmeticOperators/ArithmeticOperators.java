dc2a9a52a51bb9ff1ad6b89781444dd3a68a5fd8
unknown <nurlanbekuulutemirbek@gmail.com>
2023-03-13 23:28:01 -0500

    BankAccount.java package

---
+++ ArithmeticOperators/ArithmeticOperators.java
@@ -0,0 +1,43 @@
+package ArithmeticOperators;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        /*
+        = assignment operator
+
+        == equal equal operator (compares two variables if they are equal or not)
+        ! = not equeal opearotr ( compares if two variables are not equal)
+        bul kurstan chygyp dagy kaisyl kurska kiresin, devopes ka kirein desen dagy akchan aga jetpeit.
+
+
+
+         */
+//        System.out.println();
+
+//        int number1 = 10;
+//        int number2 = 8;
+//        System.out.println(number2==number1);
+//        System.out.println(100 == 100);
+
+//        boolean isEqual = 100 ==101;
+//        System.out.println("isEqual = " + isEqual);
+//        //soutv=last , sout= full,empty souf= string
+        
+        boolean isEven; 
+//        System.out.println("isEven = " + isEven);
+
+
+        
+        
+
+
+
+
+
+
+    }
+
+}
